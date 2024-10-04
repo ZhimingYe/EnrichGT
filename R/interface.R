238deb57d572bd50880220299c1f04a5f5046429
ZhimingYe <zhiming.ye@qq.com>
2024-10-05 03:18:03 +0800

    prepare for release 0.1.2. add unit tests

--- R/interface.R
+++ R/interface.R
@@ -5,7 +5,10 @@
 #' @param ClusterNum how many cluster will be clustered
 #' @param P.adj p.adjust cut-off. To avoid slow visualization, you can make stricter p-cut off.
 #' @param ... Others options.
-#' For an ORA result, c("ID","Description","GeneRatio","pvalue","p.adjust","geneID","Count") should be contained; For GSEA, c("ID","Description","NES","pvalue","p.adjust","core_enrichment") should be contain. For `compareClusterResult`, a `compareClusterResult` object or a data-frame with additional `Cluster` column should be contained, others similar to ORA result.
+#'
+#'
+#' @details
+#'  For an ORA result, c("ID","Description","GeneRatio","pvalue","p.adjust","geneID","Count") should be contained; For GSEA, c("ID","Description","NES","pvalue","p.adjust","core_enrichment") should be contain. For `compareClusterResult`, a `compareClusterResult` object or a data-frame with additional `Cluster` column should be contained, others similar to ORA result.
 #' @return a `gt` great table object or a `List` containing multiple `gt` objects for `compareClusterResult`.
 #' @export
 #'
