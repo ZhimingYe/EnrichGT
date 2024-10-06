a47f5683f8fb1c87772a4d6059d95a9202471faa
ZhimingYe <zhiming.ye@qq.com>
2024-10-07 02:39:16 +0800

    gen obj fun

--- R/objFrame.R
+++ R/objFrame.R
@@ -1,5 +1,5 @@
 setClass("EnrichGT_obj",slots=list(enriched_result="data.frame",
-                                         gt_object="list",
+                                         gt_object="gt_tbl",
                                          gene_modules="list",
                                          pathway_clusters="list"))
 
@@ -9,10 +9,21 @@ setMethod("show", "EnrichGT_obj", function(object) {
 })
 
 new.egt <- function(x1,x2,x3,x4){
-  objegt<-new("EnrichGT_obj")
-  objegt@enriched_result <- x1
-  objegt@gt_object <- x2
-  objegt@gene_modules <- x3
-  objegt@pathway_clusters <- x4
+  require("gt")
+  flag0<-F
+  tryCatch({
+    objegt<-new("EnrichGT_obj")
+    objegt@enriched_result <- x1
+    objegt@gt_object <- x2
+    objegt@gene_modules <- x3
+    objegt@pathway_clusters <- x4
+    flag0<-T
+  },error=function(e){
+    message("Failed to create EnrichGT object! Please re-check your input.")
+    flag0<-F
+  })
+  if(flag0==F){
+    objegt<-NULL
+  }
   return(objegt)
 }
