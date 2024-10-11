eb6b18123ad5d8dae6bd1b6171979fcbc186e2d5
ZhimingYe <zhiming.ye@qq.com>
2024-10-12 02:10:30 +0800

    simplify message

--- R/messages.R
+++ R/messages.R
@@ -1,7 +1,11 @@
 
 message_egt<-function(x,Type=0){
-  cli_h1("EnrichGT message:")
-  cli_alert_info(x)
+  if(Type==0){
+    cli_alert_info(x)
+  }
+  if(Type==1){
+    cli_alert_danger(x)
+  }
 }
 
 nsimp<-function(){
