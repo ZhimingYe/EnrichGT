93064c410055130aaa9a9212ca8c1b394727559d
ZhimingYe <zhiming.ye@qq.com>
2024-10-12 01:17:59 +0800

    EnrichGT_obj support containg clustering trees

--- R/objFrame.R
+++ R/objFrame.R
@@ -1,14 +1,15 @@
 setClass("EnrichGT_obj",slots=list(enriched_result="data.frame",
                                          gt_object="gt_tbl",
                                          gene_modules="list",
-                                         pathway_clusters="list"))
+                                         pathway_clusters="list",
+                                         clustering_tree="hclust"))
 
 
 setMethod("show", "EnrichGT_obj", function(object) {
   print(object@gt_object)
 })
 
-new.egt <- function(x1,x2,x3,x4){
+new.egt <- function(x1,x2,x3,x4,x5){
   require("gt")
   flag0<-F
   tryCatch({
@@ -17,6 +18,7 @@ new.egt <- function(x1,x2,x3,x4){
     objegt@gt_object <- x2
     objegt@gene_modules <- x3
     objegt@pathway_clusters <- x4
+    objegt@clustering_tree <- x5
     flag0<-T
   },error=function(e){
     message_egt("Failed to create EnrichGT object! Please re-check your input.")
