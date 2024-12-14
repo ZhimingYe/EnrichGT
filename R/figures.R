aeadf14eb531e298922ed9a0a478b860f88886f8
ZhimingYe <zhiming.ye@qq.com>
2024-12-15 03:37:58 +0800

    feature complete!

--- R/figures.R
+++ R/figures.R
@@ -1,5 +1,38 @@
-egtDotPlot <- function(x,...){
-  if(sum(colnames(kk@enriched_result)=="NES")==0){
+#' Visualize results generated form `EnrichGT()` using ggplot2
+#'
+#' @param x an EnrichGT object
+#' @param ntop Show top N in each cluster
+#' @param low.col the color for the lowest
+#' @param hi.col the color for the highest
+#' @param ...
+#'
+#' @returns a ggplot2 object
+#' @export
+#' @importFrom ggplot2 fortify
+#' @importFrom ggplot2 aes
+#' @importFrom ggplot2 ggplot
+#' @importFrom ggplot2 aes_string
+#' @importFrom ggplot2 geom_point
+#' @importFrom ggplot2 scale_color_gradient
+#' @importFrom ggplot2 scale_color_continuous
+#' @importFrom ggplot2 scale_fill_continuous
+#' @importFrom ggplot2 xlab
+#' @importFrom ggplot2 ylab
+#' @importFrom ggplot2 labs
+#' @importFrom ggplot2 ggtitle
+#' @importFrom ggplot2 scale_y_discrete
+#' @importFrom ggplot2 guides
+#' @importFrom ggplot2 guide_legend
+#' @importFrom ggplot2 guide_colorbar
+#' @importFrom ggplot2 facet_grid
+#' @importFrom ggplot2 scale_size
+#' @importFrom ggplot2 theme_bw
+#' @importFrom ggplot2 geom_col
+#' @importFrom methods is
+#' @importFrom forcats fct_reorder
+#' @author Zhiming Ye
+egtPlot <- function(x,...){
+  if(sum(colnames(x@enriched_result)=="absNES")==0){
     figure0<-ORA2dp(x,...)
   }else{
     figure0<-GSEA2dp(x,...)
@@ -7,40 +40,52 @@ egtDotPlot <- function(x,...){
   return(figure0)
 }
 
-ORA2dp<-function(x,clusterType="Empty",showStart=1,showEnd=20,orderBy,col_low="#1e6091",col_high="#99d98c",...){
-  # TODO max top
-  require(forcats)
-  px<-ggplot(kk@enriched_result,aes(x=PCT, y=forcats::fct_reorder(Description,kk@enriched_result$PCT), size=Count, color=Padj))+geom_point()+scale_color_continuous(low="#78cfe5", high="#ff6f81", name = "adjustedP",guide=guide_colorbar(reverse=F))+scale_size(range=c(2, 8))+xlab("Gene Ratio")+ylab("Gene Set")+theme_bw()+facet_grid(Cluster~.,scales="free",space="free_y")
+ORA2dp<-function(x,ntop = 7,low.col="#78cfe5",hi.col="#ff6f81",...){
+  if(is.list(x)){
+    cli::cli_abort("For a list object, please run plotting for every object inside list, instead of the whole list.")
+  }
+  tryCatch({
+    if(dim(x@enriched_result)[1]<2 | sum(colnames(x@enriched_result)=="Count")==0){
+      cli::cli_abort("ERROR! ")
+    }
+  },error=function(e){
+    cli::cli_abort("Not EnrichGT object! Please run `EnrichGT()` first.")
+  })
+  tryCatch({
+    df <- x@enriched_result %>%
+      group_by(Cluster) %>%
+      slice_min(order_by = Count, n = ntop, with_ties = FALSE) %>%
+      ungroup()
+  },error=function(e){
+    cli::cli_alert_warning("Subset ERROR! ")
+    df <-x@enriched_result
+  })
+
+  px<-ggplot(df,aes(x = PCT, y = fct_reorder(Description, PCT), size=Count, color=Padj))+geom_point()+scale_color_continuous(low=low.col, high=hi.col, name = "adjustedP",guide=guide_colorbar(reverse=F))+scale_size(range=c(2, 8))+xlab("Gene Ratio")+ylab("Gene Sets")+facet_grid(Cluster~.,scales="free",space="free_y")+theme_bw()
   return(px)
 }
 
-GSEA2dp<-function(x,clusterType="Empty",showTerms,col_low="#1e6091",col_high="#99d98c",...){
-  require(forcats)
-  clusterType <- match.arg(clusterType, c("MatrixResult", "NetworkResult","Empty"))
-  orderBy<-"NES"
-  if(sum(c("GSCluster","NetworkCluster")%in%colnames(x@Results))==0|clusterType=="Empty"){
-    FigDF<-data.frame(Terms=paste0(x@Results$Database,":",x@Results$Description),pValue=x@Results$p.adjust,NES=x@Results$NES,Cluster=NA)
+GSEA2dp<-function(x,ntop = 7,low.col="#78cfe5",hi.col="#ff6f81",...){
+  if(is.list(x)){
+    cli::cli_abort("For a list object, please run plotting for every object inside list, instead of the whole list.")
   }
-  if("GSCluster"%in%colnames(x@Results)&clusterType=="MatrixResult"){
-    FigDF<-data.frame(Terms=paste0(x@Results$Database,":",x@Results$Description),pValue=x@Results$p.adjust,NES=x@Results$NES,Cluster=x@Results$GSCluster)
-    FigDF$Cluster[is.na(FigDF$Cluster)]<-"UnKnown"
-  }
-  if("NetworkCluster"%in%colnames(x@Results)&clusterType=="NetworkResult"){
-    FigDF<-data.frame(Terms=paste0(x@Results$Database,":",x@Results$Description),pValue=x@Results$p.adjust,NES=x@Results$NES,Cluster=x@Results$NetworkCluster)
-    FigDF$Cluster[is.na(FigDF$Cluster)]<-"UnKnown"
-  }
-  if(nrow(FigDF)<2){
-    stop("Enrichment result is too few!\n")
-  }
-  FigDF<-FigDF%>%dplyr::arrange(desc(!!sym(orderBy)))
-  if(!(length(showTerms)>1)){
-    stop("In GSEA, please pass a vector to showTerms to determine which items will show. \n")
-  }
-  keep<-c(1:nrow(FigDF))[c(1:nrow(FigDF))%in%showTerms]
-  if(length(keep)==0){
-    stop("NO overlap!\n")
-  }
-  p<-ggplot(FigDF[keep,],aes(x=NES, y=fct_reorder(Terms,NES),fill=pValue))+geom_col()+scale_fill_continuous(low=col_low, high=col_high, name = "adjustedP",guide=guide_colorbar(reverse=F))+xlab("NES")+ylab("Gene Set")+theme_bw()
-  print(p)
-  return(p)
+  tryCatch({
+    if(dim(x@enriched_result)[1]<2 | sum(colnames(x@enriched_result)=="absNES")==0){
+      cli::cli_abort("ERROR! ")
+    }
+  },error=function(e){
+    cli::cli_abort("Not EnrichGT object! Please run `EnrichGT()` first.")
+  })
+  tryCatch({
+    df <- x@enriched_result %>%
+      group_by(Cluster) %>%
+      slice_min(order_by = absNES, n = ntop, with_ties = FALSE) %>%
+      ungroup()
+  },error=function(e){
+    cli::cli_alert_warning("Subset ERROR! ")
+    df <-x@enriched_result
+  })
+  df$NES<-ifelse(df$Reg=="UpReg",df$absNES*(1),df$absNES*(-1))
+  px<-ggplot(df,aes(x = NES, y = fct_reorder(Description, absNES), fill=Padj))+geom_col()+scale_fill_continuous(low=low.col, high=hi.col, name = "adjustedP",guide=guide_colorbar(reverse=F))+scale_size(range=c(2, 8))+xlab("Normalize Enrichment Score(NES)")+ylab("Gene Sets")+facet_grid(Cluster~.,scales="free",space="free_y")+theme_bw()
+  return(px)
 }
