ef6be4d537af2e0fb29cba0513f5a51f465bf8c3
ZhimingYe <zhiming.ye@qq.com>
2024-12-17 04:01:27 +0800

    add ORA function

--- R/cp_helpers.R
+++ R/cp_helpers.R
@@ -95,3 +95,68 @@ BuildMultigroupDEGlist<-function(Gene,Group,OrgDB=org.Hs.eg.db){
   return(Genetable)
 }
 
+
+# gmt file reader is cited from Yu lab's gson package. Author: Guangchuang Yu
+# https://github.com/YuLab-SMU/gson/blob/main/R/GMT.R
+
+##' parse gmt file to a data.frame
+##'
+##' @title getGMTFile
+##' @description
+##' read `.gmt` files
+##'
+##' @param gmtfile gmt file
+##' @importFrom utils stack
+##' @export
+##' @return data.frame
+##' @author cited from https://github.com/YuLab-SMU/gson/blob/main/R/GMT.R
+getGMTFile <- function (gmtfile) {
+  x <- readLines(gmtfile)
+  res <- strsplit(x, "\t")
+  names(res) <- vapply(res, function(y) y[1], character(1))
+  res <- lapply(res, "[", -c(1:2))
+  ont2gene <- stack(res)
+  ont2gene <- ont2gene[, c("ind", "values")]
+  colnames(ont2gene) <- c("term", "gene")
+  return(ont2gene)
+}
+
+
+#' A C++ accelerated universal enrichment analyzer
+#'
+#' @param genes a vector of gene id
+#' @param database a database
+#' @param p_adj_methods one of "holm", "hochberg", "hommel", "bonferroni", "BH", "BY", "fdr", "none"
+#' @param p_val_cut_off adjusted pvalue cutoff on enrichment tests to report
+#' @param background_genes background genes. If missing, the all genes listed in the database
+#' @param min_geneset_size minimal size of genes annotated for testing
+#' @param max_geneset_size maximal size of genes annotated for testing
+#'
+#' @returns
+#' @export
+#' @examples
+doEnrich <- function(genes,database,p_adj_methods="BH",p_val_cut_off=0.5,background_genes=NULL,min_geneset_size=10,max_geneset_size=500,multi_cores=0){
+  if(is.character(genes)){
+    result <- doEnrich_Internal(genes,database,p_adj_methods,p_val_cut_off,background_genes,min_geneset_size,max_geneset_size)
+  }else if(is.list(genes)&multi_cores<=1){
+    result <- lapply(genes,function(x){
+      tryCatch({
+        res <- doEnrich_Internal(genes=x,database,p_adj_methods,p_val_cut_off,background_genes,min_geneset_size,max_geneset_size)
+        return(res)
+      },error=function(e){
+        return(data.frame(ERROR=e))
+      })
+    })
+  }else if(is.list(genes)&multi_cores>=2){
+    require(parallel)
+    result <- mclapply(genes,function(x){
+      tryCatch({
+        res <- doEnrich_Internal(genes=x,database,p_adj_methods,p_val_cut_off,background_genes,min_geneset_size,max_geneset_size)
+        return(res)
+      },error=function(e){
+        return(data.frame(ERROR=e))
+      })
+    },mc.cores = multi_cores)
+  }
+  return(result)
+}
---
+++ R/ora.R
@@ -0,0 +1,83 @@
+parse_gs<-function(gset){
+  colnames(gset) <- c("term", "gene")
+  gset2<-split(gset,gset$term)
+  gset2<-lapply(gset2,function(x)x$gene)
+  gset2->gene_sets
+  return(list(background_genes=0,gene_sets=gene_sets))
+}
+
+##' @importFrom qvalue qvalue
+##' @importFrom stats p.adjust
+doEnrich_Internal <- function(genes,database,p_adj_methods,p_val_cut_off,background_genes,min_geneset_size,max_geneset_size){
+  tryCatch({
+    if(ncol(database)!=2&ncol(database)!=3){
+      cli::cli_abort("Not valid database")
+    }
+  },error=function(e){cli::cli_abort("Not valid database")})
+  if(ncol(database)==3){
+    colnames(database) <- c("ID","term","gene")
+    db0 <- database[,c(1,2)]
+    database <- database[,c(2,3)]
+  }else{
+    db0 <- data.frame(ID = database$term, term = database$term)
+  }
+  db0 <- db0 |> dplyr::mutate(CheckDup = paste0(ID,term)) |> dplyr::filter(!duplicated(CheckDup)) |> dplyr::select(-CheckDup) |> dplyr::rename(TERMs = term)
+  colnames(database) <- c("term", "gene")
+
+  termCount <- table(database$term) |> as.data.frame()
+  colnames(termCount)[1] <- "TERMs"
+  termCount <- termCount |> dplyr::filter(Freq >= min_geneset_size,Freq <= max_geneset_size)
+  if(nrow(termCount)==0){
+    cli::cli_abort("No gene set match the asked gene set size ")
+  }
+  termCount$TERMs <- as.character(termCount$TERMs)
+  background_genes_<-database$gene |> table() |> names()
+  database0 <-database |> dplyr::filter(term %in% termCount$TERMs)
+
+  tgtGs<-parse_gs(database0)
+  if(!is.null(background_genes)){
+    tgtGs[[1]]<-background_genes
+  }else{
+    tgtGs[[1]]<-background_genes_
+  }
+  genes<-genes[genes%in%tgtGs[[1]]]
+
+  HittedCheck<-data.frame(gene=genes,hitted="Hitted")
+  database2 <- database0 |> left_join(HittedCheck,by="gene")
+  database2 <- database2[!is.na(database2$hitted),]
+  database2 <- database2[database2$hitted=="Hitted",]
+  hitted_result <- database2 |>
+    group_by(term) |>
+    summarise(gene_list = paste(gene, collapse = "/")) |>
+    ungroup()
+  colnames(hitted_result)[1] <- "TERMs"
+  hitted_result$TERMs <- as.character(hitted_result$TERMs)
+
+
+  if(length(genes)==0){
+    cli::cli_abort("No gene overlap! Please recheck. ")
+  }
+  result <- ora_analysis(tgtGs[[2]], genes, tgtGs[[1]])
+  result$TERMs <- tgtGs[[2]] |> names()
+  df00 <- hitted_result |> left_join(result,by="TERMs") |> left_join(termCount,by="TERMs") |> left_join(db0,by="TERMs")
+  df00$padj<-p.adjust(df00$PValue,method = p_adj_methods)
+  qobj <- tryCatch(qvalue(p=df00$padj, lambda=0.05, pi0.method="bootstrap"), error=function(e) NULL)
+  if (inherits(qobj, "qvalue")) {
+    qvalues <- qobj$qvalues
+  } else {
+    qvalues <- NA
+  }
+  df00$qvalues<-qvalues
+  df00$LengthOfInput<-length(genes)
+  df00$LengthOfBG<-length(tgtGs[[1]])
+  res <- data.frame(ID=df00$ID,
+                    Description=df00$TERMs,
+                    GeneRatio=paste0(df00$Overlap,"/",df00$LengthOfInput),
+                    BgRatio=paste0(df00$Freq,"/",df00$LengthOfBG),
+                    pvalue=df00$PValue,
+                    p.adjust=df00$padj,
+                    qvalue=df00$qvalues,
+                    geneID=df00$gene_list,
+                    Count=df00$Overlap)
+  return(res)
+}
