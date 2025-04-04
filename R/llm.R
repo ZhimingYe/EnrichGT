4bfd76bdca3e0b826a7fb5a2a7aa5cfbb750e243
ZhimingYe <zhiming.ye@qq.com>
2025-04-05 02:28:25 +0800

    Add more functions to LLM

--- R/llm.R
+++ R/llm.R
@@ -1,8 +1,10 @@
 promptList <- list(
   getModuleSummary_Chinese = "这是一些生物学富集分析后重新再次整理后得到的生物学主题列表。你是一名专业生物学家,帮我用一些话概括这些通路的主题是什么,反映了什么,有什么生物学解释的角度。请简洁有力,必要时提供依据。请您还需要为这个模块拟定一个代表性名字。请使用学术写作风格。请控制字数在250字左右,避免长篇大论。请输出纯文本,不包括markdown格式。参与这个模块的通路包括:<PATHWAYLIST>",
-  getGeneSummary_Chinese = "这是一系列Gene,可能具有类似生物学功能。前面已经总结他们的生物学功能可能包括```biofuns\n<BIOFUNS>\n```\n,帮我总结探索这些基因可能潜在的生物学角色,哪一些更加值得关注,提出您的见解。请不要分点,写成一段,请控制字数在250字左右。请输出纯文本,不包括markdown格式。这些Gene包括:<GENELIST>",
+  getGeneSummary_Chinese = "这是一系列Gene,可能具有类似生物学功能。前面已经总结他们的生物学功能可能包括\n```biofuns\n<BIOFUNS>\n```\n,帮我总结探索这些基因可能潜在的生物学角色,哪一些更加值得关注,提出您的见解。请不要分点,写成一段,请控制字数在250字左右。请输出纯文本,不包括markdown格式。这些Gene包括:<GENELIST>",
+  getTitle_Chinese = "你是一名生物学家,我提供给你一段生物学文本\n``` text\n <PRERES> \n```\n,请帮我的生物学主题拟定一个title。请您只返回标题本身,只返回最优的唯一(注意是唯一)标题,请不要返回任何其他的内容.",
   getModuleSummary_English = "Here is a list of reorganized biological themes derived from enrichment analysis. As a professional biologist, please concisely summarize the overarching theme of these pathways, their biological implications, and possible interpretations. Provide evidence where necessary. Also, propose a representative name for this module. Use an academic writing style and limit the response to ~250 words. Output plain text only (no markdown). The included pathways are:<PATHWAYLIST>",
-  getGeneSummary_English = "Here is a set of genes that may share similar biological functions. Previous analysis suggests their potential functions may include ```biofuns\n<BIOFUNS>\n```\n. As a professional biologist, please help summarize their potential biological roles, highlight which ones may be most noteworthy, and provide your insights. Write in a single paragraph (~250 words) without bullet points. Output plain text only (no markdown). The genes include:<GENELIST>"
+  getGeneSummary_English = "Here is a set of genes that may share similar biological functions. Previous analysis suggests their potential functions may include \n```biofuns\n<BIOFUNS>\n```\n. As a professional biologist, please help summarize their potential biological roles, highlight which ones may be most noteworthy, and provide your insights. Write in a single paragraph (~250 words) without bullet points. Output plain text only (no markdown). The genes include:<GENELIST>",
+  getTitle_English = "You are a biologist. I will provide you with a piece of biological text. \n``` text\n <PRERES> \n```\n Please help me generate a title for the biological topic. Only return the title itself—choose the single best (and only one) title. Do not return any other content."
 )
 
 retry_function <- function(FUN, ntry = 3, delay = 3, ...) {
@@ -45,10 +47,13 @@ chatter <- function(x, Prompt) {
 }
 wrong_llm <- function() {
   cli::cli_alert_info(
-    "The LLM support of `EnrichGT` is based on package `ellmer`"
+    "The LLM support of `EnrichGT` is based on package `ellmer`\n For Example: "
+  )
+  cli::cli_code(
+    "library(ellmer)\nchat <- chat_deepseek(api_key = YOUR_API, model = \"deepseek-chat\", system_prompt = \"\")\n# Please check https://ellmer.tidyverse.org/index.html for details"
   )
   cli::cli_abort(
-    "Please check package `ellmer` installation or your model provider & API key. We can't get any results from this `chat` object. Please check https://ellmer.tidyverse.org/index.html for your LLM model registration and provide it properly in EnrichGT. "
+    "LLM model registration Failed"
   )
 }
 
@@ -92,11 +97,13 @@ summarize_clusters <- function(x, chat, prompt_type = "English") {
 
 
 summarize_genes <- function(x, y, chat, prompt_type = "English") {
-  excludeIndex <- (-1)
+  have_exclude <- F
+  excludeIndex <- c()
   if (!inherits(x, "EnrichGT_obj")) {
     cli::cli_abort("Input must be an EnrichGT_obj object")
   }
   if (sum(sapply(y, is.null)) > 0) {
+    have_exclude <- T
     excludeIndex <- which(sapply(y, is.null))
     x@gene_modules <- x@gene_modules[-excludeIndex]
     y <- y[-excludeIndex]
@@ -140,5 +147,58 @@ summarize_genes <- function(x, y, chat, prompt_type = "English") {
       )
     }
   )
-  results
+  return(list(
+    results = results,
+    have_exclude = have_exclude,
+    excludeIndex = excludeIndex
+  ))
 }
+
+summarize_title <- function(x, chat, prompt_type = "English") {
+  have_exclude <- F
+  excludeIndex <- c()
+  if (sum(sapply(x, is.null)) > 0) {
+    have_exclude <- T
+    excludeIndex <- which(sapply(x, is.null))
+    x <- x[-excludeIndex]
+    excludeIndex_warning <- paste(excludeIndex, collapse = ", ")
+    cli::cli_alert_danger(glue::glue(
+      "In cluster summarize, {excludeIndex_warning} were faild to fetch LLM summary, in next steps they will be excluded. "
+    ))
+  }
+  prompt <- switch(
+    prompt_type,
+    "English" = promptList$getTitle_English,
+    "Chinese" = promptList$getTitle_Chinese,
+    cli::cli_abort("Invalid prompt_type, must be 'English' or 'Chinese'")
+  )
+  cluster_names <- length(x)
+  results <- lapply(
+    cli::cli_progress_along(1:cluster_names, name = "Summarizing title"),
+    function(cluster) {
+      tryCatch(
+        {
+          cluster_prompt <- gsub(
+            "<PRERES>",
+            paste(x[[cluster]], collapse = ", "),
+            prompt
+          )
+          chatter(chat, cluster_prompt)
+        },
+        error = function(e) {
+          cli::cli_alert_warning(
+            glue::glue("Failed to process cluster {cluster}. ")
+          )
+          NULL
+        }
+      )
+    }
+  )
+  return(list(
+    results = results,
+    have_exclude = have_exclude,
+    excludeIndex = excludeIndex
+  ))
+}
+
+
