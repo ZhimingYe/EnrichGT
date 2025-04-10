5b9c869747830a747e7bcc7ccd271bf4ae1da458
ZhimingYe <zhiming.ye@qq.com>
2025-04-10 17:20:54 +0800

    Fix R CMD check warnings

---
+++ R/zzz.R
@@ -0,0 +1,16 @@
+# This file declares global variables to avoid R CMD check NOTE
+utils::globalVariables(c(
+  "Cluster",
+  "cluster",
+  "pvalue",
+  "NES",
+  "ID",
+  "Description",
+  "core_enrichment",
+  "absNES",
+  "Reg",
+  "Padj",
+  "Count",
+  "UpGenes",
+  "DownGenes"
+))
