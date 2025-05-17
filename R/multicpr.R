289f7b8de8b7d2214fb72d72c7d3f3237b6970e2
ZhimingYe <zhiming.ye@qq.com>
2025-05-17 12:08:29 +0800

    split R6 obj & remove `pheatmap` deps

--- R/multicpr.R
+++ R/multicpr.R
@@ -1,25 +1,39 @@
+egt_comparison_reactor <- function(Type = NULL) {
+  error00 <- function(){
+    cli::cli_alert_danger(
+      "When create a comparison analysis object, please pinpoint the type of this reactor: "
+    )
+    cli::cli_code(
+      "comparison_object <- egt_comparison_reactor('ORA')\ncomparison_object <- egt_comparison_reactor('GSEA')"
+    )
+    cli::cli_abort("Undefined reactor type")
+  }
+  if (is.null(Type) | sum(Type %in% c("ORA", "ora", "GSEA", "gsea")) != 1) {
+    error00()
+  } else {
+    if (Type == "ORA" | Type == "ora"){
+      kk <- comparison_reactor_ora$new()
+    } else if(Type == "GSEA" | Type == "gsea"){
+      kk <- comparison_reactor_gsea$new()
+    } else {
+      error00()
+    }
+  }
+  kk
+}
+
+
 #' @importFrom R6 R6Class
-egt_mcp <- R6::R6Class(
-  "comparison_reactor",
+comparison_reactor_base <- R6::R6Class(
+  "comparison_reactor_base",
   public = list(
     initialize = function(Type = NULL) {
-      if (Type %in% c("ORA", "ora")) {
-        private$type <- "ORA"
-        private$group_name <- tibble::tibble(
-          Group = "Initial",
-          Index = "G_0",
-          Included = F
-        )
-      } else if (Type %in% c("GSEA", "gsea")) {
-        private$type <- "GSEA"
-        private$group_name <- tibble::tibble(
-          Group = "Initial",
-          Index = "G_0",
-          Included = F
-        )
-      } else {
-        cli::cli_abort("Please define correct analysis type: `ORA` or `GSEA`. ")
-      }
+      cli::cli_h1("EnrichGT comparison reactor")
+      private$group_name <- tibble::tibble(
+        Group = "Initial",
+        Index = "G_0",
+        Included = F
+      )
     },
     append_enriched_result = function(x, group) {
       if (length(group) > 1) {
@@ -42,8 +56,7 @@ egt_mcp <- R6::R6Class(
 
       private$raw_enriched_result -> bb
       tdf <- cpres_internal_getter(x) |>
-        dplyr::filter(!duplicated(Description)) |>
-        dplyr::mutate(p_Adj = -log10(p.adjust))
+        .egt_mcp_helper_wash_p_val(type = "default")
       if (nrow(tdf) < 10) {
         cli::cli_abort("Invalid input. ")
       }
@@ -52,45 +65,6 @@ egt_mcp <- R6::R6Class(
 
       private$group_name <- aa
       private$raw_enriched_result <- bb
-      invisible(self)
-    },
-    make_plans = function(x = "auto", use_value = "p") {
-      private$check_appended_data()
-      if (x[1] == "auto" & length(x) == 1) {
-        private$plan <- private$group_name$Group
-        private$plan <- private$plan[private$plan != "Initial"]
-      } else if (sum(x %in% private$group_name$Group) == length(x)) {
-        private$plan <- x
-      } else {
-        cli::cli_abort("Please re-check your plan. Can't find enough groups. ")
-      }
-      x <- private$group_name[
-        private$group_name$Group %in% private$plan,
-        "Index",
-        drop = T
-      ]
-      private$raw_enriched_result -> list0
-      if (use_value == "NES" & private$type == "GSEA") {
-        use_what0 <- "NES"
-      } else {
-        use_what0 <- "p_Adj"
-      }
-      a1 <- lapply(x, function(q) {
-        d1 <- list0[[q]] |> dplyr::select(CPRID, !!sym(use_what0))
-        colnames(d1)[2] <- paste0(q, "|", use_value)
-        d1
-      })
-      # print(a1)
-      a2 <- Reduce(
-        function(a, b) {
-          a <- as.data.frame(a)
-          b <- as.data.frame(b)
-          dplyr::full_join(a, b, by = "CPRID")
-        },
-        a1
-      )
-      a2[is.na(a2)] <- 0 # may have bugs
-      private$agg_df <- a2
       invisible(self)
     },
     summarize = function() {
@@ -108,16 +82,6 @@ egt_mcp <- R6::R6Class(
       names(list1) <- names(list0)
       private$raw_enriched_result <- list1
       cli::cli_alert_success(glue::glue("Filter according to p adjust < {x}"))
-      invisible(self)
-    },
-    prefilter_by_NES = function(x = 1) {
-      private$check_appended_data()
-      if (private$type != "GSEA") cli::cli_abort("Not GSEA! ")
-      private$raw_enriched_result -> list0
-      list1 <- lapply(list0, function(q) q |> dplyr::filter(abs(NES) > x))
-      names(list1) <- names(list0)
-      private$raw_enriched_result <- list1
-      cli::cli_alert_success(glue::glue("Filter according to NES > {x}"))
       invisible(self)
     },
     find_relationship = function(
@@ -150,7 +114,7 @@ egt_mcp <- R6::R6Class(
         "Clustering with {dist_method} distance and {hclust_method} linkage, cut into {Num} clusters"
       ))
       print(table(clusters))
-      pheatmap::pheatmap(
+      base_pheatmap(
         mat,
         cluster_rows = hc,
         cutree_rows = Num,
@@ -208,24 +172,172 @@ egt_mcp <- R6::R6Class(
           "Please add data into reactor by using YOUR_reactor_object$append_enriched_result(x = YOUR RESULT, group = FROM WHICH GROUP)"
         )
       }
+    },
+    make_plans_internal = function(x, use_value) {
+      private$check_appended_data()
+      if (x[1] == "auto" & length(x) == 1) {
+        private$plan <- private$group_name$Group
+        private$plan <- private$plan[private$plan != "Initial"]
+      } else if (sum(x %in% private$group_name$Group) == length(x)) {
+        private$plan <- x
+      } else {
+        cli::cli_abort("Please re-check your plan. Can't find enough groups. ")
+      }
+      x <- private$group_name[
+        private$group_name$Group %in% private$plan,
+        "Index",
+        drop = T
+      ]
+      private$raw_enriched_result -> list0
+      use_what0 <- .egt_mcp_helper_get_col(
+        use_value = use_value,
+        type = private$type
+      )
+      a1 <- lapply(x, function(q) {
+        if (sum(colnames(list0[[q]]) %in% use_what0) < 1)
+          cli::cli_abort(glue::glue(
+            "Please check the input, can't find column: {use_what0}"
+          ))
+        d1 <- list0[[q]] |> dplyr::select(CPRID, !!sym(use_what0))
+        colnames(d1)[2] <- paste0(q, "|", use_what0)
+        d1
+      })
+      # print(a1)
+      a2 <- Reduce(
+        function(a, b) {
+          a <- as.data.frame(a)
+          b <- as.data.frame(b)
+          dplyr::full_join(a, b, by = "CPRID")
+        },
+        a1
+      )
+      a2[is.na(a2)] <- 0 # may have bugs
+      private$agg_df <- a2
+      # invisible(self)
     }
   )
 )
-egt_comparison_reactor <- function(Type = NULL) {
-  if (is.null(Type) | sum(Type %in% c("ORA", "ora", "GSEA", "gsea")) != 1) {
-    cli::cli_alert_danger(
-      "When create a comparison analysis object, please pinpoint the type of this reactor: "
-    )
-    cli::cli_code(
-      "comparison_object <- egt_comparison_reactor('ORA')\ncomparison_object <- egt_comparison_reactor('GSEA')"
-    )
-    cli::cli_abort("Undefined reactor type")
-  } else {
-    kk <- egt_mcp$new(Type)
+
+comparison_reactor_ora <- R6::R6Class(
+  "comparison_reactor_ora",
+  inherit = comparison_reactor_base,
+  public = list(
+    initialize = function() {
+      super$initialize()
+      private$type <- "ORA"
+    },
+    make_plans = function(x = "auto", use_value = "p") {
+      private$make_plans_internal(x = x, use_value = use_value)
+      invisible(self)
+    }
+  ),
+  private = list()
+)
+
+comparison_reactor_gsea <- R6::R6Class(
+  "comparison_reactor_gsea",
+  inherit = comparison_reactor_base,
+  public = list(
+    initialize = function() {
+      super$initialize()
+      private$type <- "GSEA"
+    },
+    make_plans = function(x = "auto", use_value = "NES") {
+      private$make_plans_internal(x = x, use_value = use_value)
+      invisible(self)
+    },
+    prefilter_by_NES = function(x = 1) {
+      private$check_appended_data()
+      if (private$type != "GSEA") cli::cli_abort("Not GSEA! ")
+      private$raw_enriched_result -> list0
+      list1 <- lapply(list0, function(q) q |> dplyr::filter(abs(NES) > x))
+      names(list1) <- names(list0)
+      private$raw_enriched_result <- list1
+      cli::cli_alert_success(glue::glue("Filter according to NES > {x}"))
+      invisible(self)
+    }
+  ),
+  private = list()
+)
+
+.egt_mcp_helper_wash_p_val <- function(x, type) {
+  if (type == "default") {
+    x |>
+      dplyr::filter(!duplicated(Description)) |>
+      dplyr::mutate(p_Adj = -log10(p.adjust))
   }
-  kk
+  x
+  # Can add more filtering way
 }
 
+.egt_mcp_helper_get_col <- function(use_value, type) {
+  if (use_value == "NES" & type == "GSEA") {
+    use_what0 <- "NES" # Already contains
+  } else if (sum(use_value %in% c("p", "pval", "P", "Padj", "pAdj")) > 0) {
+    use_what0 <- "p_Adj" # Created in .egt_mcp_helper_wash_p_val fun
+  }
+  # This should return the agg_df 's true contained row names
+  use_what0
+}
+
+
+base_pheatmap <- function(
+  mat,
+  cluster_rows    = NULL, 
+  cluster_cols    = FALSE,
+  cutree_rows     = NULL, 
+  show_rownames   = TRUE,
+  show_colnames   = TRUE,
+  color           = heat.colors(100),
+  legend          = TRUE,
+  legend_pos      = "topright",
+  ...
+){
+  if (!is.null(cluster_rows) && inherits(cluster_rows, "hclust")) {
+    rdend <- stats::as.dendrogram(cluster_rows)
+  } else {
+    rdend <- NA
+  }
+  if (!is.null(cutree_rows) && inherits(cluster_rows, "hclust")) {
+    row_groups  <- stats::cutree(cluster_rows, k = cutree_rows)
+    side_colors <- grDevices::rainbow(cutree_rows)[row_groups]
+  } else {
+    side_colors <- NULL
+  }
+  labRow <- if (show_rownames) rownames(mat) else NA
+  labCol <- if (show_colnames) colnames(mat) else NA
+  stats::heatmap(
+    mat,
+    Rowv          = rdend,
+    Colv          = if (cluster_cols) NULL else NA,
+    labRow        = labRow,
+    labCol        = labCol,
+    col           = color,
+    RowSideColors = side_colors,
+    ...
+  )
+  if (legend && !is.null(cutree_rows) && inherits(cluster_rows, "hclust")) {
+    legend(
+      legend_pos,
+      legend = paste0("Cluster ", seq_len(cutree_rows)),
+      fill   = rainbow(cutree_rows),
+      border = NA,
+      bty    = "n"
+    )
+  }
+}
+
+
+# # 创建测试数据
+# set.seed(123)
+# mat <- matrix(rnorm(100), 10000, 5)
+# rownames(mat) <- paste0("Gene", 1:10000)
+# colnames(mat) <- paste0("Sample", 1:5)
+
+# # 绘制热图
+# base_pheatmap(mat, cluster_rows = TRUE, cutree_rows = 3)
+
+# kkk <- comparison_reactor_ora$new()
 # kk <- egt_comparison_reactor("ORA")
 # kk$append_enriched_result(ora_result1, "group_1_go")
 # kk$append_enriched_result(ora_result3, "group_2_go")
